6c69193f10d8bbaddcdff0208fe6720a0ef2f0fb
AngelNz21 <angel.nunez5219@alumnos.udg.mx>
2024-11-05 00:02:16 -0600

    lazy chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainHome from "./components/main-home";
-import FirebaseAuth from "./components/firebase-auth";
-import FlowerRecognition from "./components/flower-recognition";
+import { Suspense, lazy } from "react";
+
+const MainHome = lazy(() => import("./components/main-home"));
+const FirebaseAuth = lazy(() => import("./components/firebase-auth"));
+const FlowerRecognition = lazy(() => import("./components/flower-recognition"));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MainHome />} />
-        <Route path="/auth" element={<FirebaseAuth />} />
-        <Route path="/flower-recognition" element={<FlowerRecognition />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainHome />} />
+          <Route path="/auth" element={<FirebaseAuth />} />
+          <Route path="/flower-recognition" element={<FlowerRecognition />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
